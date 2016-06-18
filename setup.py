6fbc5b4b960da9d43564e61a1714b896aa603672
Andrea Bisognin <andrea.bisognin@gmail.com>
2016-06-18 22:35:57 +0200

    added setup stuff

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-galaxy',
+    version='0.1',
+    description='Old google-code project',
+    author='dont know',
+    author_email='dontkwnow@dontknow.com',
+    url='http://code.google.com/p/django-galaxy/',
+    download_url='',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
